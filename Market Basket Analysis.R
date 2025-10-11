04c34fb0f9c4da8630536584c73be523b266c87b
Anthony A. Ataupah <145671960+A-Arthur-A@users.noreply.github.com>
2025-10-11 10:29:01 +0700

    Update Market Basket Analysis.R

--- Market Basket Analysis.R
+++ Market Basket Analysis.R
@@ -18,3 +18,9 @@ inspect(subset(mba, lift>1))
 
 #Menampilkan jumlah kombinasi dari produk yang terdapat pada daftar transaksi yang ada
 inspect(apriori(transaksi, parameter=list(support=.1, minlen=2, target="frequent itemset")))
+
+#Membaca transaksi dari file data_transaksi2.txt
+transaksi <- read.transactions(file="https://storage.googleapis.com/dqlab-dataset/data_transaksi2.txt", format="single", sep="\t", cols=c(1,2), skip=1)
+
+#Menampilkan jumlah kombinasi dari produk yang terdapat pada daftar transaksi yang ada
+inspect(apriori(transaksi, parameter = list(support=.03, minlen=2, target='frequent itemsets')))
