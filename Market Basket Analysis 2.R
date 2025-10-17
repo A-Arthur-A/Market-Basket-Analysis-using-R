c3f510fa7c63794bf19fa54419b1a75c890e9065
Anthony A. Ataupah <145671960+A-Arthur-A@users.noreply.github.com>
2025-10-17 19:58:25 +0700

    Update Market Basket Analysis 2.R

--- Market Basket Analysis 2.R
+++ Market Basket Analysis 2.R
@@ -1,4 +1,15 @@
+#Menggunakan library arules
 library(arules)
+
+#Membaca transaksi dari file data_transaksi.txt
 transaksi <- read.transactions(file="https://storage.googleapis.com/dqlab-dataset/data_transaksi.txt", format="single", sep="\t", cols=c(1,2), skip=1)
 
+#Menampilkan daftar item transaksi
 transaksi@itemInfo
+
+#Menampilkan daftar kode transaksi
+transaksi@itemsetInfo
+
+#Menampilkan transaksi dalam bentuk matrix
+transaksi@data
+
