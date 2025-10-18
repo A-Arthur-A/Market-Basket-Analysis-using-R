178caa6f0d68a049f8a283fe9e5e41992f9fdac1
Anthony A. Ataupah <145671960+A-Arthur-A@users.noreply.github.com>
2025-10-19 02:11:41 +0700

    Update Market Basket Analysis 2.R

--- Market Basket Analysis 2.R
+++ Market Basket Analysis 2.R
@@ -16,3 +16,12 @@ transaksi@data
 #Menampilkan jumlah kemunculan item pada keseluruhan transaksi
 data_item <- itemFrequency(transaksi, type="absolute")
 data_item
+
+#Melakukan sorting pada data_item
+data_item <- sort(data_item, decreasing = TRUE)
+
+#Mengambil 3 item pertama
+data_item <- data_item[1:3]
+
+#Konversi data_item menjadi data frame dengan kolom Nama_Produk dan Jumlah
+data_item <- data.frame("Nama Produk"=names(data_item), "Jumlah"=data_item, row.names=NULL)
